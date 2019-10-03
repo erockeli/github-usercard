ed46dd002b23168adb706d4fa62fa0af4148a942
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-03 15:15:06 -0500

    Creating axios to send a Get request to the link

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,47 @@
            https://api.github.com/users/<your name>
 */
 
+function axiosFunction(username){
+  axios
+  .get(`https://api.github.com/users/${username}`)
+  .then(response => {
+    //response is my object
+    // console.log(response);
+    cardGenerator(response.data);
+    // resolve(response);
+    return username;
+  })
+  .then(username =>{
+    axios.get(`https://api.github.com/users/${username}/followers`)
+    .then(response=>{
+      // console.log(response);
+      let friendList = response.data;
+
+         friendList.forEach((item)=>{
+
+        console.log(item.login);
+
+                axios
+          .get(`https://api.github.com/users/${item.login}`)
+          .then(response => {
+            
+            cardGenerator(response.data);
+            
+          })
+
+      });
+
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
